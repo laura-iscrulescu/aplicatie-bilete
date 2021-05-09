1556626ff47a8008bc47972db41c608386d8327d
danisafta <saftamailbox@gmail.com>
2021-05-09 14:05:35 +0300

    Provided more details on how the api should look

--- services/app/controllers/api.py
+++ services/app/controllers/api.py
@@ -2,18 +2,37 @@ from flask import Blueprint
 
 controllers_bp = Blueprint('controllers_bp', __name__)
 
-@controllers_bp.route('/seats', methods = ["GET"])
-def seats():
-    return "Hello 1!"
-
 @controllers_bp.route('/shows', methods = ["GET"])
 def shows():
+    ''' Returns details about all the shows available '''
     return "Hello 2!"
 
-@controllers_bp.route('/book', methods = ["GET"])
+@controllers_bp.route('/show/<string:show>', methods = ["GET"])
+def show(show):
+    ''' Returns details about the show provided in the request '''
+    return "Hello 2!"
+
+@controllers_bp.route('/seats/<show>/<int:day>', methods = ["GET"])
+def seats(show, day):
+    ''' Return details about the seats for that show '''
+    return "Hello 1!"
+
+@controllers_bp.route('/book', methods = ["POST"])
 def book():
+    ''' 
+        Book a seat at a show
+        @arg1 : show
+        @arg2 : day
+        @arg3 : seat
+    '''
     return "Hello 3!"
 
-@controllers_bp.route('/cancel', methods = ["GET"])
+@controllers_bp.route('/cancel', methods = ["POST"])
 def cancel():
+    ''' 
+        Book a seat at a show
+        @arg1 : show
+        @arg2 : day
+        @arg3 : seat
+    '''
     return "Hello 3!"
