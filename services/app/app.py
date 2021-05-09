69c61ecdf75188952a77c8b169d6cbef233226b5
danisafta <saftamailbox@gmail.com>
2021-05-09 14:05:35 +0300

    Provided more details on how the api should look

--- services/app/app.py
+++ services/app/app.py
@@ -6,7 +6,7 @@ from controllers.api import controllers_bp
 app = Flask(__name__)
 json = FlaskJSON(app)
 
-app.register_blueprint(controllers_bp, url_prefix='/controllers')
+app.register_blueprint(controllers_bp, url_prefix='/api')
 
 if __name__ == "__main__":
     app.run(debug=True, host = "0.0.0.0")
